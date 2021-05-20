5647b9e5dcc02821479aa1057ecc21948025014f
William Iommi <william.iommi@gmail.com>
2021-05-21 01:11:12 +0200

    added btn on bottom

--- components/Btn.jsx
+++ components/Btn.jsx
@@ -2,16 +2,16 @@ import { memo, useCallback, useContext } from "react";
 import { AppContext } from "../lib/appContext";
 import { TOGGLE_MODAL } from "../lib/useAppReducer";
 
-const RulesBtn = ({ onClick }) => {
+const Btn = ({ text, onClick }) => {
   return (
     <button
       type="button"
       onClick={onClick}
       className="py-2 px-10 border transition-all duration-500 border-grayLight rounded-md text-grayLight font-body text-lg hover:bg-grayLight hover:text-DarkText"
     >
-      RULES
+      {text}
     </button>
   );
 };
 
-export default memo(RulesBtn);
+export default memo(Btn);
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,19 @@
 import { useCallback, useContext } from "react";
 import { AppContext } from "../lib/appContext";
-import { TOGGLE_GAME, TOGGLE_MODAL } from "../lib/useAppReducer";
+import { RESET_GAME, TOGGLE_GAME, TOGGLE_MODAL } from "../lib/useAppReducer";
 import useLoadPoints from "../lib/useLoadPoints";
 import Modal from "../components/Modal";
-import RulesBtn from "../components/RulesBtn";
+import Btn from "../components/Btn";
 import GameSelector from "../components/GameSelector";
 import Picker from "../components/Picker";
-
-import { LIZARD, PAPER, ROCK, SCISSORS, SPOCK } from "../lib/utils";
 import ScoreBoard from "../components/ScoreBoard";
 
 export default function Home() {
   const { state, dispatch } = useContext(AppContext);
+  const resetGame = useCallback(
+    () => dispatch({ type: RESET_GAME }),
+    [dispatch]
+  );
   const toggleModal = useCallback(
     () => dispatch({ type: TOGGLE_MODAL }),
     [dispatch]
@@ -42,7 +44,10 @@ export default function Home() {
             }
           />
           <Picker isClassicGame={state.isClassicGame} />
-          <RulesBtn onClick={toggleModal} />
+          <div className="flex flex-row justify-center space-x-5 pr-5 mb-10 desktop:justify-end">
+            <Btn text="CHANGE GAME" onClick={resetGame} />
+            <Btn text="RULES" onClick={toggleModal} />
+          </div>
           <Modal
             isVisible={state.isRulesOpen}
             isClassicGame={state.isClassicGame}
