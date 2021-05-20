c4f41551ca773187d91fc432c467244489e4e267
William Iommi <william.iommi@gmail.com>
2021-05-20 18:16:08 +0200

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,10 +8,31 @@ module.exports = {
     },
     colors: {
       transparent: "transparent",
+      white: '#ffffff',
       grayLight: "#f5f5f5",
       DarkText: "hsl(229, 25%, 31%)",
       ScoreText: "hsl(229, 64%, 46%)",
       HeaderOutline: "hsl(217, 16%, 45%)",
+      rock: {
+        light: '#DF405B',
+        dark: '#911B39',
+      },
+      paper: {
+        light: '#5572F4',
+        dark: '#2F4496',
+      },
+      scissors: {
+        light: '#ECA726',
+        dark: '#BA6F2B',
+      },
+      lizard: {
+        light: '#8D5DE5',
+        dark: '#6438B1',
+      },
+      spock: {
+        light: '#51BDD0',
+        dark: '#47809B',
+      },
     },
     screens: {
       desktop: "1024px",
