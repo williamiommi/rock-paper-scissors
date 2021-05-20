c8d4005b9198af3fbecd2913c5927bfccd93ddd3
William Iommi <william.iommi@gmail.com>
2021-05-20 11:00:19 +0200

    context added

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,12 @@
+import { AppProvider } from "../lib/appContext";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <AppProvider>
+      <Component {...pageProps} />;
+    </AppProvider>
+  );
 }
 
 export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,13 @@ module.exports = {
       body: ["Barlow Semi Condensed", "sans-serif"],
     },
     colors: {
+      grayLight: "#f5f5f5",
       DarkText: "hsl(229, 25%, 31%)",
       ScoreText: "hsl(229, 64%, 46%)",
       HeaderOutline: "hsl(217, 16%, 45%)",
+    },
+    screens: {
+      desktop: "1024px",
     },
     extend: {},
   },
