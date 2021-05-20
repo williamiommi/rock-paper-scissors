14b9ba185d118b7052d6166d9d58790e2cf2b3c4
William Iommi <william.iommi@gmail.com>
2021-05-20 16:19:43 +0200

    point from localestorage

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,22 @@
 import { useCallback, useContext } from "react";
-import Modal from "../components/Modal";
 import { AppContext } from "../lib/appContext";
 import { TOGGLE_GAME, TOGGLE_MODAL } from "../lib/useAppReducer";
+import Modal from "../components/Modal";
+import RulesBtn from "../components/RulesBtn";
+import useLoadPoints from "../lib/useLoadPoints";
 
 export default function Home() {
   const { state, dispatch } = useContext(AppContext);
-  const toggleModal = useCallback(() => dispatch(TOGGLE_MODAL), [dispatch]);
+  const toggleModal = useCallback(
+    () => dispatch({ type: TOGGLE_MODAL }),
+    [dispatch]
+  );
+  // load initial points
+  useLoadPoints();
   return (
     <div className="text-center">
       rock paper scissors
-      <button type="button" onClick={toggleModal}>
-        Open Modal
-      </button>
+      <RulesBtn onClick={toggleModal} />
       <Modal
         isVisible={state.isRulesOpen}
         isClassicGame={state.isClassicGame}
