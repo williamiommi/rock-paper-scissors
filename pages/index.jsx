1790d279c763969dfa94191ab4fb7014dd876562
William Iommi <william.iommi@gmail.com>
2021-05-20 18:22:44 +0200

    minor on ui

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
 import { useCallback, useContext } from "react";
 import { AppContext } from "../lib/appContext";
 import { TOGGLE_GAME, TOGGLE_MODAL } from "../lib/useAppReducer";
+import useLoadPoints from "../lib/useLoadPoints";
 import Modal from "../components/Modal";
 import RulesBtn from "../components/RulesBtn";
-import useLoadPoints from "../lib/useLoadPoints";
+import Token from "../components/Token";
+
+
+import { LIZARD, PAPER, ROCK, SCISSORS, SPOCK } from '../lib/utils';
 
 export default function Home() {
   const { state, dispatch } = useContext(AppContext);
@@ -17,6 +21,15 @@ export default function Home() {
     <div className="text-center">
       rock paper scissors
       <RulesBtn onClick={toggleModal} />
+      <Token type={ROCK} />
+      <br /><br />
+      <Token type={PAPER} />
+      <br /><br />
+      <Token type={SCISSORS} />
+      <br /><br />
+      <Token type={LIZARD} />
+      <br /><br />
+      <Token type={SPOCK} />
       <Modal
         isVisible={state.isRulesOpen}
         isClassicGame={state.isClassicGame}
