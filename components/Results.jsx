1b098dcce8ff644c4b94a1c94ea389e4b0e3a4f7
William Iommi <william.iommi@gmail.com>
2021-05-21 17:46:33 +0200

    minor on calculate winner

--- components/Results.jsx
+++ components/Results.jsx
@@ -1,8 +1,17 @@
-import { memo } from "react";
-import { getHousePick } from "../lib/utils";
+import { memo, useEffect, useState } from "react";
+import { getHousePick, getWinner, WIN } from "../lib/utils";
 import Token from "./Token";
 
-const Results = ({ isClassicGame, userPick }) => {
+const Results = ({ isClassicGame, userPick, handleWin }) => {
+  const [housePick, setHousePick] = useState(getHousePick(isClassicGame));
+
+  useEffect(() => {
+    if (housePick) {
+      const res = getWinner(userPick, housePick, isClassicGame);
+      if (res === WIN) handleWin();
+    }
+  }, [housePick]);
+
   return (
     <div className="flex flex-col mx-5 mb-20 desktop:mx-auto desktop:w-full desktop:max-w-2xl">
       <div className="flex flex-row mx-auto w-full justify-between items-center mt-16 text-center order-2 desktop:order-1">
@@ -21,7 +30,7 @@ const Results = ({ isClassicGame, userPick }) => {
         </div>
         <div className="flex w-1/2 justify-center items-center">
           <span className="transform scale-50 tablet:scale-75 desktop:scale-100">
-            <Token type={getHousePick(isClassicGame)} isDisabled />
+            <Token type={housePick} isDisabled />
           </span>
         </div>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,7 @@
 import { useCallback, useContext } from "react";
 import { AppContext } from "../lib/appContext";
 import {
+  REGISTER_WIN,
   RESET_GAME,
   TOGGLE_GAME,
   TOGGLE_MODAL,
@@ -13,6 +14,7 @@ import GameSelector from "../components/GameSelector";
 import Picker from "../components/Picker";
 import ScoreBoard from "../components/ScoreBoard";
 import Results from "../components/Results";
+import { setWin } from "../lib/utils";
 
 export default function Home() {
   const { state, dispatch } = useContext(AppContext);
@@ -32,6 +34,10 @@ export default function Home() {
     (payload) => dispatch({ type: USER_PICK, payload }),
     [dispatch]
   );
+  const handleWin = useCallback(() => {
+    setWin(state.isClassicGame);
+    dispatch({ type: REGISTER_WIN });
+  }, [dispatch]);
   // load initial points
   useLoadPoints();
   return (
@@ -56,7 +62,11 @@ export default function Home() {
           {!state.userPick ? (
             <Picker isClassicGame={state.isClassicGame} onClick={userPick} />
           ) : (
-            <Results isClassicGame={state.isClassicGame} userPick={state.userPick} />
+            <Results
+              isClassicGame={state.isClassicGame}
+              userPick={state.userPick}
+              handleWin={handleWin}
+            />
           )}
           {/* <div className="flex flex-row justify-center space-x-5 mb-10 desktop:justify-end desktop:pr-5">
             <Btn text="CHANGE GAME" onClick={resetGame} />
