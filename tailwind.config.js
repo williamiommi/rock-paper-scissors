39de1d83566543722de2c8dcac132d8277ef88ed
William Iommi <william.iommi@gmail.com>
2021-05-21 15:23:10 +0200

    minor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}", "./lib/**/*.{js,ts,jsx,tsx}"],
+  purge: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./lib/**/*.{js,ts,jsx,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     fontFamily: {
@@ -8,36 +12,36 @@ module.exports = {
     },
     colors: {
       transparent: "transparent",
-      white: '#ffffff',
+      white: "#ffffff",
       grayLight: "#f5f5f5",
       DarkText: "hsl(229, 25%, 31%)",
       ScoreText: "hsl(229, 64%, 46%)",
       HeaderOutline: "hsl(217, 16%, 45%)",
-      emptyToken: '#0f1729',
+      emptyToken: "#0f1729",
       rock: {
-        light: '#DF405B',
-        dark: '#911B39',
+        light: "#DF405B",
+        dark: "#911B39",
       },
       paper: {
-        light: '#5572F4',
-        dark: '#2F4496',
+        light: "#5572F4",
+        dark: "#2F4496",
       },
       scissors: {
-        light: '#ECA726',
-        dark: '#BA6F2B',
+        light: "#ECA726",
+        dark: "#BA6F2B",
       },
       lizard: {
-        light: '#8D5DE5',
-        dark: '#6438B1',
+        light: "#8D5DE5",
+        dark: "#6438B1",
       },
       spock: {
-        light: '#51BDD0',
-        dark: '#47809B',
+        light: "#51BDD0",
+        dark: "#47809B",
       },
     },
     screens: {
-      mobile: '375px',
-      tablet: '768px',
+      mobile: "375px",
+      tablet: "768px",
       desktop: "1024px",
     },
     extend: {},
