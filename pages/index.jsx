1041e3ae846c1129090485978ef026d08f10108d
William Iommi <william.iommi@gmail.com>
2021-05-23 13:00:06 +0200

    minor

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react";
+import Head from "next/head";
 import { AppContext } from "../lib/appContext";
 
 // import hooks
@@ -28,46 +29,63 @@ export default function Home() {
     playAgain,
   } = useFunctions();
   return (
-    <div className="flex flex-col">
-      {!state.isGameSelected && (
-        <GameSelector
-          isGameSelected={state.isGameSelected}
-          onSelectGame={toggleGame}
-        />
-      )}
+    <>
+      <Head>
+        <title>Rock, Paper, Scissors Game</title>
+      </Head>
+      <div className="flex flex-col">
+        {!state.isGameSelected && (
+          <GameSelector
+            isGameSelected={state.isGameSelected}
+            onSelectGame={toggleGame}
+          />
+        )}
 
-      {state.isGameSelected && (
-        <>
-          <ScoreBoard
-            isClassicGame={state.isClassicGame}
-            points={
-              state.isClassicGame
-                ? state.pointsGameClassic
-                : state.pointsGameBonus
-            }
-          />
-          {!state.userPick ? (
-            <Picker isClassicGame={state.isClassicGame} onClick={userPick} />
-          ) : (
-            <Results
+        {state.isGameSelected && (
+          <>
+            <ScoreBoard
               isClassicGame={state.isClassicGame}
-              userPick={state.userPick}
-              handleWin={handleWin}
-              onPlayAgain={playAgain}
+              points={
+                state.isClassicGame
+                  ? state.pointsGameClassic
+                  : state.pointsGameBonus
+              }
             />
-          )}
-          <div className="flex flex-col items-center justify-center space-y-5 w-52 mx-auto mb-10 desktop:w-auto desktop:flex-row desktop:justify-end desktop:space-y-0 desktop:space-x-5 desktop:pr-5 desktop:mx-0">
-            <Btn text="CHANGE GAME" onClick={resetGame} className="w-full desktop:w-auto" />
-            <Btn text="RESET POINTS" onClick={resetPoints} className="w-full desktop:w-auto" />
-            <Btn text="RULES" onClick={toggleModal} className="w-full desktop:w-auto" />
-          </div>
-          <Modal
-            isVisible={state.isRulesOpen}
-            isClassicGame={state.isClassicGame}
-            onCloseModal={toggleModal}
-          />
-        </>
-      )}
-    </div>
+            {!state.userPick ? (
+              <Picker isClassicGame={state.isClassicGame} onClick={userPick} />
+            ) : (
+              <Results
+                isClassicGame={state.isClassicGame}
+                userPick={state.userPick}
+                handleWin={handleWin}
+                onPlayAgain={playAgain}
+              />
+            )}
+            <div className="flex flex-col items-center justify-center space-y-5 w-52 mx-auto mb-10 desktop:w-auto desktop:flex-row desktop:justify-end desktop:space-y-0 desktop:space-x-5 desktop:pr-5 desktop:mx-0">
+              <Btn
+                text="CHANGE GAME"
+                onClick={resetGame}
+                className="w-full desktop:w-auto"
+              />
+              <Btn
+                text="RESET POINTS"
+                onClick={resetPoints}
+                className="w-full desktop:w-auto"
+              />
+              <Btn
+                text="RULES"
+                onClick={toggleModal}
+                className="w-full desktop:w-auto"
+              />
+            </div>
+            <Modal
+              isVisible={state.isRulesOpen}
+              isClassicGame={state.isClassicGame}
+              onCloseModal={toggleModal}
+            />
+          </>
+        )}
+      </div>
+    </>
   );
 }
